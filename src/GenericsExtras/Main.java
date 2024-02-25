23942b9b633a8bf495da45c0801532fbda566b89
kaiismith <ThangDD.BI12-406@st.usth.edu.vn>
2024-02-25 18:42:55 +0700

    GenericsExtras

---
+++ src/GenericsExtras/Main.java
@@ -0,0 +1,4 @@
+package GenericsExtras;
+
+public class Main {
+}
---
+++ src/GenericsExtras/util/QueryItem.java
@@ -0,0 +1,5 @@
+package GenericsExtras.util;
+
+public interface QueryItem {
+    public boolean matchFieldValue(String fieldName, String value);
+}
