d35774a32bfcb016e63ed220d5ac3476303211ff
kaiismith <ThangDD.BI12-406@st.usth.edu.vn>
2024-02-25 18:41:46 +0700

    GenericClass

---
+++ src/GenericClass/Mappable.java
@@ -0,0 +1,13 @@
+package GenericClass;
+
+public interface Mappable {
+    void render();
+
+    static double[] stringToLatLon(String location) {
+        var splits = location.split(",");
+        double lat = Double.valueOf(splits[0]);
+        double lng = Double.valueOf(splits[1]);
+        return new double[]{lat, lng};
+    }
+}
+
